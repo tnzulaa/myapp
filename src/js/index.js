92dea7aeb52f66f3933362007f59b949720b2b0c
SunZul <tnzulaa@gmail.com>
2020-10-28 16:45:32 +0800

    hailtiin ur dung uzuulne

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,31 @@
 require("@babel/polyfill"); // компайл хийж чадахгүй функц кодуудыг оруулдаг
 import Search from './model/Search';
+import { elements } from './view/base';
+import * as searchView from './view/searchView';
 
 
 const controlSearch = async () => {
     const state = {};
 
 // 1) Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
-    const query = 'pizza';
+    const query = searchView.getInput();
 if(query){
     // 2) Шинээр хайлтын обьектыг үүсгэж өгнө.
     state.search = new Search(query);
     // 3) Хайлт хийхэд зориулж дэлгэцийн UI бэлтгэнэ.
+    searchView.clearSearchQuery();
+    searchView.clearSearchResult();
     // 4) Хайлтыг гүйцэтгэнэ.
     await state.search.doSearch();
     // 4) Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
-    console.log(state.search.result);
+    
+    searchView.renderRecipes(state.search.result);
     
 }
 
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
